08d96da96170a69fa44c8a1b41a8ad245037663b
rainightRoot <156874524+rainightRoot@users.noreply.github.com>
2024-02-23 10:25:45 +0800

    Create babylon-install.sh

---
+++ babylon-install.sh
@@ -0,0 +1,73 @@
+MONIKER="equinox"
+sudo apt -q update
+sudo apt -qy install curl git jq lz4 build-essential
+sudo apt -qy upgrade
+sudo rm -rf /usr/local/go
+curl -Ls https://go.dev/dl/go1.20.14.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
+eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
+# Clone project repository
+cd $HOME
+rm -rf babylon
+git clone https://github.com/babylonchain/babylon.git
+cd babylon
+git checkout v0.8.3
+
+# Build binaries
+make build
+
+# Prepare binaries for Cosmovisor
+mkdir -p $HOME/.babylond/cosmovisor/genesis/bin
+mv build/babylond $HOME/.babylond/cosmovisor/genesis/bin/
+rm -rf build
+
+# Create application symlinks
+sudo ln -s $HOME/.babylond/cosmovisor/genesis $HOME/.babylond/cosmovisor/current -f
+sudo ln -s $HOME/.babylond/cosmovisor/current/bin/babylond /usr/local/bin/babylond -f
+
+# Download and install Cosmovisor
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.5.0
+
+# Create service
+sudo tee /etc/systemd/system/babylon.service > /dev/null << EOF
+[Unit]
+Description=babylon node service
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=$HOME/.babylond"
+Environment="DAEMON_NAME=babylond"
+Environment="UNSAFE_SKIP_BACKUP=true"
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/.babylond/cosmovisor/current/bin"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable babylon.service
+
+# Set node configuration
+babylond config chain-id bbn-test-2
+babylond config keyring-backend test
+babylond config node tcp://localhost:16457
+
+# Initialize the node
+babylond init $MONIKER --chain-id bbn-test-3
+
+# Download genesis and addrbook
+curl -Ls http://89.58.62.213/genesis.json > $HOME/.babylond/config/genesis.json
+curl -Ls http://89.58.62.213/addrbook.json > $HOME/.babylond/config/addrbook.json
+curl -Ls http://89.58.62.213/config.toml > $HOME/.babylond/config/config.toml
+curl -Ls http://89.58.62.213/app.toml > $HOME/.babylond/config/app.toml
+
+
+
+# Set custom ports
+sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:16458\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:16457\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:16460\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:16456\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":16466\"%" $HOME/.babylond/config/config.toml
+sed -i -e "s%^address = \"tcp://localhost:1317\"%address = \"tcp://0.0.0.0:16417\"%; s%^address = \":8080\"%address = \":16480\"%; s%^address = \"localhost:9090\"%address = \"0.0.0.0:16490\"%; s%^address = \"localhost:9091\"%address = \"0.0.0.0:16491\"%; s%:8545%:16445%; s%:8546%:16446%; s%:6065%:16465%" $HOME/.babylond/config/app.toml
+sudo systemctl start babylon.service
